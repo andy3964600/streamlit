94bd978958d20320feb2683ce08d7d2869c4a187
Chia-Hao Tang <p7476762@gmail.com>
2021-11-10 14:56:26 +0800

    Add files via upload

--- stock_1.py
+++ stock_1.py
@@ -1,10 +1,7 @@
 import yfinance as yf
 import streamlit as st
 import datetime
-import talib
-import ta
 import pandas as pd
-import requests
 import matplotlib.pyplot as plt
 import mpl_finance as mpf
 yf.pdr_override()
@@ -46,16 +43,6 @@ st.line_chart(data['Adj Close'])
 # Volume
 st.bar_chart(tickerDF.Volume)
 
-# ## SMA and EMA
-# Simple Moving Average
-data['SMA'] = talib.SMA(data['Adj Close'], timeperiod=20)
-
-# Exponential Moving Average
-data['EMA'] = talib.EMA(data['Adj Close'], timeperiod=20)
-
-# Plot
-st.header(f"Simple Moving Average vs. Exponential Moving Average\n {company_name}")
-st.line_chart(data[['Adj Close', 'SMA', 'EMA']])
 
 # KBar
 st.header(f"KBar\n {company_name}")
@@ -71,42 +58,18 @@ mpf.candlestick2_ochl(ax, tickerDF['Open'], tickerDF['Close'], tickerDF['High'],
                       tickerDF['Low'], width=0.6, colorup='r', colordown='g', alpha=0.6)
 st.pyplot(fig)
 # Bollinger Bands
-data['upper_band'], data['middle_band'], data['lower_band'] = talib.BBANDS(
-    data['Adj Close'], timeperiod=20)
+
 
 # Plot
-st.header(f"Bollinger Bands\n {company_name}")
-st.line_chart(data[['Adj Close', 'upper_band', 'middle_band', 'lower_band']])
 
 # ## MACD (Moving Average Convergence Divergence)
 # MACD
-data['macd'], data['macdsignal'], data['macdhist'] = talib.MACD(
-    data['Adj Close'], fastperiod=12, slowperiod=26, signalperiod=9)
-
 # Plot
-st.header(f"Moving Average Convergence Divergence\n {company_name}")
-st.line_chart(data[['macd', 'macdsignal']])
-
-# CCI (Commodity Channel Index)
 # CCI
-cci = ta.trend.cci(data['High'], data['Low'], data['Close'], n=31, c=0.015)
-
 # Plot
-st.header(f"Commodity Channel Index\n {company_name}")
-st.line_chart(cci)
-
 # ## RSI (Relative Strength Index)
 # RSI
-data['RSI'] = talib.RSI(data['Adj Close'], timeperiod=14)
-
 # Plot
-st.header(f"Relative Strength Index\n {company_name}")
-st.line_chart(data['RSI'])
-
 # ## OBV (On Balance Volume)
 # OBV
-data['OBV'] = talib.OBV(data['Adj Close'], data['Volume'])/10**6
-
 # Plot
-st.header(f"On Balance Volume\n {company_name}")
-st.line_chart(data['OBV'])
